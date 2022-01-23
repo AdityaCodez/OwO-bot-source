364acbf0bb32e75084d12a62637d1c462dca2c87
AdityaCodez <97006414+AdityaCodez@users.noreply.github.com>
2022-01-23 10:45:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+client.on('ready', () => {
+
+client.user.setStatus('dnd')
+client.user.setActivity(`$help`), ({ type: 'WATCHING' })
+
+});
+
+//Dnd, idle, online status discord.js v13 
+
+
+
+var http = require('http'); http.createServer(function (req, res) { res.write("I'm alive"); res.end(); }).listen(8080); 
+
+Just put this code in index.js and run no server.js file needed! 
+
+
+
+Const mainfile = ("./OwO/main.js")
